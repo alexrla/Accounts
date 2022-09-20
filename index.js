29178adf6fa132536c7637ca2ddf495243311df7
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-09-20 17:49:44 -0300

    Definindo as operações do sistema

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ import chalk from "chalk";
 
 // Core modules
 import fs from 'fs';
+
+const operations = () => {
+
+    inquirer.prompt([{
+
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+            "Criar conta",
+            "Consultar saldo",
+            "Depositar",
+            "Sacar",
+            "Sair"
+        ]
+
+    }])
+    .then((answer) => {
+
+        const action = answer["action"];
+
+        console.log(action);
+
+    })
+    .catch((error) => console.log(error));
+
+};
+
+operations();
